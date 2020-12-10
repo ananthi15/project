0d860fdda361b6059df7ae9ba3c28485c45d7972
ananthi15 <71563945+ananthi15@users.noreply.github.com>
2020-12-10 12:50:55 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/cstack',{useNewUrlParser:true,useUnifiedTopology:true})
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
+
+var login = require('./routes/login');
+var register= require('./routes/register');
+//var users = require('./routes/users');
+var main = require('./routes/main');
+const { setTimeout } = require('timers');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('/', login);
+app.use('/register',register);
+app.use('/main', main);
+// app.use('/employees', employees);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
